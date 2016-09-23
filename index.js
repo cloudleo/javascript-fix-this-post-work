bd2e76868ede334fd1c02e0fefc239daa459b0a5
leonardo <l.castillo1989@Hotmail.com>
2016-09-23 02:30:12 -0400

    this apply call

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var cake = {
   decorate: function(updateFunction) {
     var status = "Decorating with " + this.topping + ". Ready to eat soon!"
     updateFunction(status)
-    setTimeout(function() {
-      updateFunction(serve.apply(this, "Happy Eating!", this.customer))
+    setTimeout(() => {
+      updateFunction(serve.apply(this, ["Happy Eating!", this.customer]))
     }, 2000)
   }
 }
@@ -23,14 +23,16 @@ var pie = {
   customer: "Tammy"
 }
 
+
 function makeCake() {
-  var updateCakeStatus;
-  mix(updateCakeStatus)
+  var updateCakeStatus = updateStatus.bind(this);
+  mix.call(cake,updateCakeStatus)
 }
 
 function makePie() {
-  var updatePieStatus;
-  mix(updatePieStatus)
+  pie.decorate = cake.decorate.bind(pie);
+  var updatePieStatus = updateStatus.bind(this);
+  mix.call(pie,updatePieStatus)
 }
 
 function updateStatus(statusText) {
@@ -39,22 +41,24 @@ function updateStatus(statusText) {
 
 function bake(updateFunction) {
   var status = "Baking at " + this.bakeTemp + " for " + this.bakeTime
-  setTimeout(function() {
-    cool(updateFunction)
+  updateFunction(status)
+  setTimeout(() => {
+    cool.call(this, updateFunction)
   }, 2000)
 }
 
 function mix(updateFunction) {
   var status = "Mixing " + this.ingredients.join(", ")
-  setTimeout(function() {
-    bake(updateFunction)
+  setTimeout(() => {
+    bake.call(this,updateFunction)
   }, 2000)
   updateFunction(status)
 }
 
 function cool(updateFunction) {
   var status = "It has to cool! Hands off!"
-  setTimeout(function() {
+  updateFunction(status)
+  setTimeout(() => {
     this.decorate(updateFunction)
   }, 2000)
 }
@@ -62,6 +66,11 @@ function cool(updateFunction) {
 function makeDessert() {
   //add code here to decide which make... function to call
   //based on which link was clicked
+  if(this === document.getElementsByClassName("js-make")[0]){
+    makeCake.call(document.getElementById("cake"))
+  }else{
+   makePie.call(document.getElementById("pie"))
+  }
 }
 
 function serve(message, customer) {
